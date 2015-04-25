5deccfee8a5aa98fdc40fbe1e657b4dff0665742
SujeethKumar <sujeethkumar17@gmail.com>
2015-04-25 19:17:13 -0400

    added create and insert dummy

--- src/com/controller/PasswordChangeContrl.java
+++ src/com/controller/PasswordChangeContrl.java
@@ -5,7 +5,6 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
